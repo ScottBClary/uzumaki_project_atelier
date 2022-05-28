3e4971ccaa9dd947356026e42b2b1c57e3456ded
Amanda <amandaqwright@gmail.com>
2022-05-28 14:16:47 -0500

    have questions and answers from api rendering

--- src/QandA/MainQA.js
+++ src/QandA/MainQA.js
@@ -6,29 +6,15 @@ import QAList from './QAList.js';
 import AddQModal from './AddQModal.js';
 import axios from 'axios';
 import '../QandAstyle.css';
-// require('dotenv').config();
-// import API_TOKEN from '../../.env';
-// import dotenv from 'dotenv';
-// dotenv.config();
-
 
 class MainQA extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       questionModalOpened: false,
-      results: [{
-        question: "something",
-        answers: ['hey','hey', 'hey']
-      },
-      {
-        question: "something",
-        answers: ['hey','hey', 'hey']
-      },
-      {
-        question: "something",
-        answers: ['hey','hey', 'hey']
-      }],
+      results: [],
+      question: '',
+      answers: {},
       productId: 66642
 
     };
@@ -39,13 +25,16 @@ class MainQA extends React.Component {
   componentDidMount() {
     axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/qa/questions?product_id=${this.state.productId}`, {
       headers: {
-        'Authorization': `Bearer ${process.env.API_TOKEN}`
+        'Authorization': process.env.API_TOKEN
       }
     })
     // axios.get('/qa/questions', {'product_id': this.state.productId, 'results': this.state.questions})
       .then((body) => {
+        console.log(body);
         this.setState({
-          results: body.data.results
+          results: body.data.results,
+          question: body.data.results.question_body,
+          answers: body.data.results.answers
         });
       })
       .catch((error) => {
@@ -65,7 +54,7 @@ class MainQA extends React.Component {
       <div>
         <h2 className='header'>QUESTIONS AND ANSWERS</h2>
         <Search/>
-        <QAList questions={this.state.results} className='qa-container'/>
+        <QAList entries={this.state.results} className='qa-container'/>
         {/* <button>More Answered Questions</button> */}
         {this.state.questionModalOpened ? <AddQModal closeQuestionModal={this.openQuestionModal}/> : null}
         <button className='add-question-button' onClick={this.openQuestionModal}>ADD QUESTION</button>
--- src/QandA/QAList.js
+++ src/QandA/QAList.js
@@ -2,13 +2,14 @@ import React from 'react';
 import OneQ from './OneQ.js';
 import OneA from './OneA.js';
 
-const QAList = function ({ questions }) {
-  const entryList = questions.map((entry, index) =>
+const QAList = function ({ entries }) {
+  const entryList = entries.map((entry, index) =>
     <ul key={index}>
-      <OneQ question={entry.question}/>
-      {entry.answers.map((answer, index) =>
+      <OneQ question={entry.question_body}/>
+      {console.log(entry)}
+      {Object.values(entry.answers).map((answer, index) =>
         <ul key={index}>
-          <OneA answer={answer}/>
+          <OneA answer={answer.body}/>
         </ul>
       )}
     </ul>
